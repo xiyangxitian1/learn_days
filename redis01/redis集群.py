5ab13fec968a40d2cb8cd7239e8a06c0b038aca6
xiyangxitian <liyan@live.shop.edu.cn>
2019-12-15 21:03:45 +0800

    redis数据库学习2

---
+++ redis01/redis集群.py
@@ -0,0 +1,19 @@
+from rediscluster import *
+
+if __name__ == '__main__':
+    try:
+        # 构建所有的节点，Redis会使用CRC16算法，将键和值写到某个节点上
+        startup_nodes = [
+            {"host": "192.168.84.128", "port": "7000"},
+            {"host": "192.168.84.128", "port": "7001"},
+            {"host": "192.168.84.128", "port": "7002"},
+        ]
+        src = RedisCluster(startup_nodes=startup_nodes, decode_responses=True)
+        # 设置为name
+        result = src.set("name", "itheima")
+        print(result)
+
+        name = src.get("name")
+        print(name)
+    except Exception as e:
+        print(e)
